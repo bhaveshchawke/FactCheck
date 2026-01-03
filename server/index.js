8192cc9c08cad34cb816864b0891d4d4971fc4d0
Bhavesh Chawke <bhaveshchawke4321@gmail.com>
2026-01-03 19:52:22 +0530

    Fix Express 5 routing issue for wildcard route

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    // Handle SPA fallback
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
